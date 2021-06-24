efdee4de58fb9f0cf9718335e36d88c70a020998
imashishks <sayhitoak@gmail.com>
2021-06-24 23:46:20 +0530

    module css added for details page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,13 +1,12 @@
-import { useRouter } from "next/router";
-import { Fragment } from "react";
+import classes from "./meetupDetail.module.css";
 const MeetupDetailsPage = () => {
   return (
-    <Fragment>
+    <section className={classes.details}>
       <img src="https://cdn.pixabay.com/photo/2018/07/14/15/27/cafe-3537801__480.jpg" />
       <h1>Dummy title</h1>
       <address> Kuch bhi generic address</address>
       <p>DEscription goes here</p>
-    </Fragment>
+    </section>
   );
 };
 export default MeetupDetailsPage;
