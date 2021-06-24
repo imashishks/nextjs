2ea87a69c68bbe64801cd2d61729816348a8b41e
imashishks <sayhitoak@gmail.com>
2021-06-24 23:05:00 +0530

    Added navigation and layout wrap to app

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const MEETUP_DATA = [
+  {
+    id: "m1",
+    title: "First Meetup",
+    image:
+      "https://cdn.pixabay.com/photo/2018/07/14/15/27/cafe-3537801__480.jpg",
+    address: "Some where on earth",
+    description: "Chill",
+  },
+  {
+    id: "m2",
+    title: "Second Meetup",
+    image:
+      "https://cdn.pixabay.com/photo/2018/01/21/01/46/architecture-3095716__340.jpg",
+    address: "Jumri tilaiya",
+    description: "Meetup and have fun",
+  },
+];
+
 const HomePage = () => {
-  return (
-    <div>
-      <p>Heryy</p>
-      <a href="/test">go to test</a>
-    </div>
-  );
+  return <MeetupList meetups={MEETUP_DATA} />;
 };
 
 export default HomePage;
--- pages/news/index.js
+++
@@ -1,5 +0,0 @@
-const NewsPage = () => {
-  return <div>News page</div>;
-};
-
-export default NewsPage;
