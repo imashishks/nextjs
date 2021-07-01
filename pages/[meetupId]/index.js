98a7792b74a266ed388ca59957fc051d992a06b9
Ashish Kumar <ashkumar66@publicisgroupe.net>
2021-07-01 23:46:05 +0530

    Changed image tag to next/imge

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,8 +1,9 @@
 import classes from "./meetupDetail.module.css";
+import Image from 'next/image'
 const MeetupDetailsPage = () => {
   return (
     <section className={classes.details}>
-      <img src="https://cdn.pixabay.com/photo/2018/07/14/15/27/cafe-3537801__480.jpg" />
+      <Image src="https://cdn.pixabay.com/photo/2018/07/14/15/27/cafe-3537801__480.jpg" alt="meetup-detail image" />
       <h1>Dummy title</h1>
       <address> Kuch bhi generic address</address>
       <p>DEscription goes here</p>
